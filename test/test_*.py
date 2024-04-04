75d598f7cc704726b705d5ba00f65608bf4e3849
aljanhan <aljanhan_smith@bloomfield.edu>
2024-04-04 15:51:15 -0400

    improve

---
+++ test/test_*.py
@@ -0,0 +1,62 @@
+import time
+from seleniumbase import BaseCase
+
+class ConTest(BaseCase):
+  def test_contact_page(self):
+      #open page
+    self.open("https://wwdev.csproject.org/")
+      
+    self.assert_title("Warewise")
+      
+    #Locate the login button by ID 
+    loginButton = self.driver.find_element("a[href=/login]")
+    loginButton.click()
+    
+    #Locate sign up button by ID
+    signUp = self.driver.find_element("button[type=submit][from=login-form]+ button")
+    signUp.click()
+    
+    
+    #Locate the input fields and submit button by ID 
+    username_input= self.driver.find_element("input[placeholder=username]")
+    
+    password_input = self.driver.find_element("input[placeholder=confrim password]")
+    
+    email_input = self.driver.find_element("input[placeholder=email]")
+
+    phone_input = self.driver.find_element("input[placeholder=phone number]")
+    
+    
+    #Enter valid data into the input fields
+    
+    username_input.send_keys("standard_user")
+    time.sleep(2)
+    
+    password_input.send_keys("secert_sauce")
+    time.sleep(2)
+    
+    confirm_input.send_keys("secret_sauce")
+    time.sleep(2)
+    
+    email_input.send_keys("emailTestAl1@example.com")
+    time.sleep(2)
+    
+    phone_input.send_keys("91123456789")
+    time.sleep(2)
+    
+    #Click the submit button for sign up
+    signUp = self.driver.find_elements("button[type=submit][form=login-form]+ button")
+    signUp.click()
+    
+    
+    # Enter login credentials and click login
+    email_input = self.driver.find_element("input[placeholder=email]")
+    password_input = self.driver.find_element("input[placeholder=password]")
+    email_input.send_keys("emailTestAl1@example.com")
+    time.sleep(2)
+    password_input.send_keys("sauce")
+    time.sleep(2)
+    
+    submit_button = self.driver.find_element("a[href=/login]")
+    submit_button.click()
+    
