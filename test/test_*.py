a1aabaf44b10256b2c94f849fa38a9cf107d145a
aljanhan <aljanhan_smith@bloomfield.edu>
2024-04-07 16:12:47 -0400

    data value

--- test/test_*.py
+++ test/test_*.py
@@ -18,12 +18,37 @@ class ConTest(BaseCase):
     loginButton.click() 
       
 #Locate sign up button by ID
-    signUp = self.driver.find_element("button[type='submit'][form'=login-form']+ button")
+    signUp = self.driver.find_element("button[type='submit'][form='login-form']+ button")
     signUp.click()
     
        #Locate the input fields and submit button by ID 
-    username_input= self.driver.find_element("input[placeholder=username]")
+    username_input= self.driver.find_element("input[placeholder='username']")
     
-    password_input = self.driver.find_element("input[placeholder=confrim password]")
+    password_input= self.driver.find_element("input[placeholder='password']")
+   
+    confirm_password_input = self.driver.find_element("input[placeholder='confirm password']")
     
-    email_input = self.driver.find_elemen
+    email_input = self.driver.find_element("input[placeholder='email']") 
+    
+    
+    phonenumber_input = self.driver.find_element("input[placeholder='phone number']")
+    
+    #Enter valid data into the input fields
+    
+    username_input.send_keys("drip")
+    time.sleep(2)
+    
+    password_input.send_keys("bloomfield")
+    time.sleep(2)
+    
+    confirm_password_input.send_keys("bloomfield")
+    time.sleep(2)
+    
+    email_input.send_keys("aljanhan_smith@bloomfield.edu")
+    time.sleep(2)
+    
+    phonenumber_input.send_keys("9735177946")
+    time.sleep(2)
+    
+    
+  
