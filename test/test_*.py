0945b891ad70173f122108b4c3d4ffe0e27ca702
aljanhan <aljanhan_smith@bloomfield.edu>
2024-04-07 17:53:04 -0400

    update

--- test/test_*.py
+++ test/test_*.py
@@ -18,7 +18,7 @@ class ConTest(BaseCase):
     loginButton.click() 
       
 #Locate sign up button by ID
-    signUp = self.driver.find_element("button[type='submit'][form='login-form']+ button")
+    signUp = self.driver.find_element("button[type='submit'][form='login-form'] + button")
     signUp.click()
     
        #Locate the input fields and submit button by ID 
@@ -36,19 +36,42 @@ class ConTest(BaseCase):
     #Enter valid data into the input fields
     
     username_input.send_keys("drip")
-    time.sleep(2)
+    time.sleep(1)
     
     password_input.send_keys("bloomfield")
-    time.sleep(2)
+    time.sleep(1)
     
     confirm_password_input.send_keys("bloomfield")
+    time.sleep(1)
+    
+    email_input.send_keys("aljanhan_smith@bloomfield.edu")
+    time.sleep(1)
+    
+    phonenumber_input.send_keys("9735177946")
+    time.sleep(1)
+    
+    #Click the button to sign up
+    
+    signUp = self.driver.find_element("button[type='submit'][form='signup-form'] + button")
+    signUp.click()
     time.sleep(2)
+    
+    #Enter login crendentals and click login
+    
+    email_input =self.driver.find_element("input[placeholder='email']")
+    
+    password_input =self.driver.find_element("input[placeholder='password']")
     
     email_input.send_keys("aljanhan_smith@bloomfield.edu")
     time.sleep(2)
     
-    phonenumber_input.send_keys("9735177946")
+    password_input.send_keys("bloomfield")
+    time.sleep(2) 
+    
+    #click login
+    
+    login_button = self.driver.find_element("a[href='/login']")
+    login_button.click()
     time.sleep(2)
-    
-    
-  
+    self.open("https://wwdev.csproject.org/")
+    time.sleep(3)
